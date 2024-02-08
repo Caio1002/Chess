e9fdcb687b9b5b369a61a73da815b54771baea9c
Caio Rodrigues <86518848+Caio1002@users.noreply.github.com>
2024-02-07 23:36:28 -0300

    Create position

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Chess.tabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess.tabuleiro;
+
+namespace Chess{
+    class program{
+        static void Main(string[] args){
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição " + P);
+
+            Console.ReadLine();
+
+        }
+    }
+}
