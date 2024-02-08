f23a76d9419e99908e8c2c2017495c9622443e03
Caio Rodrigues <86518848+Caio1002@users.noreply.github.com>
2024-02-07 23:49:31 -0300

    Starting chessboard

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -4,16 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Chess.tabuleiro;
 
 namespace Chess{
     class program{
         static void Main(string[] args){
             Posicao P;
 
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição " + P);
+            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
             Console.ReadLine();
 
